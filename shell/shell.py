56302f2bef8c7541b646c072985bb474468e5839
student <daamparan@miners.utep.edu>
2021-02-21 15:04:17 -0700

    Finished adding pipe and redirection functionality; however shell program ends after completing these two tasks

--- shell/shell.py
+++ shell/shell.py
@@ -10,7 +10,25 @@ re gives us
 #either commands or starting a process etc
 
 def reDir(param):
-    return
+    if '<' in param: #look at input file
+        leftProg = param[:param.index('<')]
+        rightProg = param[param.index('<')+1:]
+        os.close(0)
+        os.open(rightProg[0], os.O_RDONLY) #open input file and read
+        os.set_inheritable(0, True) #inherit output fd
+        param.remove('<')
+
+    else: #look for output file | same as previous case
+        leftProg = param[:param.index('>')]
+        rightProg = param[param.index('>')+1:]
+        os.close(1) #close input fd
+        os.open(rightProg[0], os.O_CREAT | os.O_WRONLY) #open prog on right with write and create if not found
+        os.set_inheritable(1, True) #inhereit output
+        param.remove('>')
+
+    if '<' in rightProg or '>' in rightProg: #anything left to redirection
+        reDir(rightProg)
+    exeProg(param)
 
 def pipe(param):
     #piping means two progs share their ifnormaion while running
@@ -18,20 +36,18 @@ def pipe(param):
     rightProg = param[param.index('|')+1:]
 
     pr, pw = os.pipe() #parent write and read
-    for f in (pr,pw): #allow child to inheret
-        os.set_inheritable(f, True)
+    rc = os.fork() #child process
 
-    rc = os,fork() #child process
-
-    #check the child | copied from exeProg 
+    #setting the inherites within each case eased BadFile descriptor error
+    #check the child | copied from exeProg
     if rc < 0:
-        os.write(2, 'fork failed, returning %d\n' %rc).encode())
+        os.write(2, ('fork failed, returning %d\n'%rc).encode())
         sys.exit(1)
-        
+
     elif rc == 0:
         os.close(1) #close output file
         os.dup(pw) #use parent write
-
+        os.set_inheritable(1, True) #inherite output write
         for fd in (pr, pw): #close the file descriptors
             os.close(fd)
         exeProg(leftProg) #compute one portion of the pipe
@@ -41,22 +57,26 @@ def pipe(param):
     else:
         os.close(0) #input file
         os.dup(pr) #use parent read
-
-        for fd in (pr, pw):
+        os.set_inheritable(0, True) #inherite input read
+        for fd in (pr, pw): #close fd
             os.close(fd)
         if '|' in rightProg: #if there is another pipe command
             pipe(rightProg) #call our pipe method
-
         exeProg(rightProg) #parent does right of pipe
+        #incase of error
         os.write(2, ('Could not execute %s\n' %rightProg[0]).encode())
         sys.exit(1)
-            
-    print(leftProg)
-    print(rightProg)
-    return
-
 
 def exeProg(param):
+    if '/' in param[0]: #running exe file such as python file
+        prog = param[0]
+        try:
+            os.execve(prog, param, os.environ)
+        except FileNotFoundError:
+            pass #fail smoothly
+        os.write(2, ("Could not exec. File not Found: %s\n" % param[0]).encode())
+        sys.exit(1) #terminate with error
+
     #directly for commands
     for dir in re.split(':', os.environ['PATH']):#go in all directories
         prog = '%s/%s'% (dir,param[0]) #get the first command
@@ -70,10 +90,9 @@ def exeProg(param):
 
 def ident_Input(userIn):
     wait_child = True #by default we always want to wait
-
     if len(userIn) <= 0:
         return #empty, no point
-    
+
     elif userIn[0].lower() == 'exit': #generic all exit input | exit always be first
         os.write(1, 'Have a good day!\n\n'.encode())
         sys.exit(1)
@@ -93,7 +112,7 @@ def ident_Input(userIn):
         pipe(userIn)
 
     elif '<' in userIn or '>' in userIn:
-        print('We are redirecting')
+        reDir(userIn)
 
     else: #other commmand from /usr/bin
         rc = os.fork() #child of shell prog
@@ -106,7 +125,7 @@ def ident_Input(userIn):
         else:
             if wait_child: #we wait for the child
                 child_Proc = os.wait() #waits on the child
-            #if ampersand we do not wait for the child 
+            #if ampersand we do not wait for the child
 
 while True: #this allows shell to always be ready for input
     if 'PS1' in os.environ: #if there is custom prompt 1 then it re prints it out
